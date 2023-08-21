d02af6af453919cd2990049ca3be88c12f8ac649
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-08-22 01:49:24 +0500

    first

---
+++ app/add-new-todo.js
@@ -0,0 +1,8 @@
+export default function AddNewTodo(){
+    return(
+<div class="mb-4 flex justify-center" >
+      <input type="text" class="border rounded p-2" placeholder="Enter your task" />
+      <button class="bg-blue-500 text-white px-4 py-2 rounded ml-2">Add</button>
+    </div>
+    )
+}
---
+++ app/todo-list.js
@@ -0,0 +1,9 @@
+export default function TodoList(){
+    return(
+<ul class="list-none pl-6">
+        <li class="mb-2  flex justify-center">Todo 1</li>
+        <li class="mb-2 flex justify-center">Todo 2</li>
+        <li class="mb-2 flex justify-center">Todo 3</li>
+      </ul>
+    )
+}
